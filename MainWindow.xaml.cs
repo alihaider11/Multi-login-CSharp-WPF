13a88bb105f4c1db0e117d6e8e9c1d7aee308ce5
Ali Haider <ali74576@gmail.com>
2020-04-20 13:16:07 +0500

    C# Code Academy

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Configuration;
+namespace Multi_Login
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+        public MainWindow()
+        {
+            InitializeComponent();
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString.ToString();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if(con.State == System.Data.ConnectionState.Open)
+            {
+                con.Close();
+            }
+            if (VerifyUser(txtUsername.Text, txtPassword.Password))
+            {
+                MessageBox.Show("Login Successfully", "Congrats", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("Username or password is incorrect", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        private bool VerifyUser(string username, string password)
+        {
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "select Status from Users where username='"+username+"' and password='"+password+"'";
+            dr = com.ExecuteReader();
+            if (dr.Read())
+            {
+                if(Convert.ToBoolean(dr["Status"]) == true)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
